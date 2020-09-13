8a230a942d9489b5c117c3ba6d0e323b04899a5b
TheAeonianDuke <abhishek18211@iiitd.ac.in>
2020-09-14 01:58:11 +0530

    Drag done. Fix hardcoded cubicbezier vector.

--- Assignment_1/src/main.cpp
+++ Assignment_1/src/main.cpp
@@ -1,5 +1,8 @@
 #include "utils.h"
 #include "math.h"
+#include <tuple>
+#include <vector>
+#include <algorithm>
 
 #define DRAW_CUBIC_BEZIER 0 // Use to switch Linear and Cubic bezier curves
 #define SAMPLES_PER_BEZIER 30 //Sample each Bezier curve as N=10 segments and draw as connected lines
@@ -7,20 +10,20 @@
 // GLobal variables
 std::vector<float> controlPoints;
 std::vector<float> linearBezier;
-std::vector<float> cubicBezier;
 std::vector<float> pointsLoc;
+std::vector<std::tuple<float,float>> placePoints;
+std::vector<float> cubicBezier;
+std::tuple<float,float> selectedCoords;
+bool pointSelected = false;
+bool pointBuffer = false;
 int points=0;
 int width = 640, height = 640; 
 bool controlPointsUpdated = false;
+bool linearBezDraw = true;
 
 float dist(int x1, int y1, int x2, int y2)
 {
     return sqrt(abs(pow(x2 - x1, 2)) + abs(pow(y2 - y1, 2)));
-}
-
-void print(float msg)
-{
-    std::cout<<"MSG: "<<msg<<std::endl;
 }
 
 void calculatePiecewiseLinearBezier()
@@ -61,8 +64,12 @@ void calculatePiecewiseLinearBezier()
 
 void calculatePiecewiseCubicBezier(bool endpoint)
 {
-    std::cout << controlPoints.size() << " " << points << std::endl;
+
     if(endpoint) {
+        std::cout << "Entered Cubic Bezier " << " " << points << std::endl;
+        if(linearBezDraw==false){
+            cubicBezier.clear();
+        }
         int sz = controlPoints.size(); // Contains 3 points/vertex. Ignore Z
         float x[4], y[4];
         float delta_t = 1.0 / (SAMPLES_PER_BEZIER - 1.0);
@@ -79,8 +86,6 @@ void calculatePiecewiseCubicBezier(bool endpoint)
         y[2] = controlPoints[i + 7];
         x[3] = controlPoints[i + 9];
         y[3] = controlPoints[i + 10];
-//        float lastDist = dist(pointsLoc[pS-2], pointsLoc[pS-1], pointsLoc[pS-4], pointsLoc[pS-3]);
-//        std::cout << "distance " << lastDist << x[2] << " " << x[3] << " "<< y[2] << " " << y[3] << " "<< std::endl;
 
         cubicBezier.push_back(x[0]);
         cubicBezier.push_back(y[0]);
@@ -100,6 +105,7 @@ void calculatePiecewiseCubicBezier(bool endpoint)
         cubicBezier.push_back(x[3]);
         cubicBezier.push_back(y[3]);
         cubicBezier.push_back(0.0);
+        linearBezDraw = true;
     }
 }
 
@@ -121,9 +127,10 @@ int main(int, char* argv[])
     glGenBuffers(1, &VBO_linearBezier);
     glGenVertexArrays(1, &VAO_linearBezier);
 
+    //TODO:
     glGenBuffers(2, &VBO_cubicBezier);
     glGenVertexArrays(2, &VAO_cubicBezier);
-    //TODO:
+
 
     int button_status = 0;
 
@@ -150,28 +157,93 @@ int main(int, char* argv[])
         glClear(GL_COLOR_BUFFER_BIT);
 
         bool endpoint = false;
-
         if(io.MouseClicked[0] && !ImGui::IsAnyItemActive()){
+
             x = io.MousePos.x;
             y = io.MousePos.y;
 
+            if (pointSelected && pointBuffer)
+            {
+                float selected_x = std::get<0>(selectedCoords);
+                float selected_y = std::get<1>(selectedCoords);
+//                std::cout<< selected_x << selected_y << "hree" << std::endl;
 
-            pointsLoc.push_back(x);
-            pointsLoc.push_back(y);
+                float rescaled_x1 = -1.0 + ((1.0*selected_x - 0) / (width - 0)) * (1.0 - (-1.0));
+                float rescaled_y1 = -1.0 + ((1.0*(height - selected_y) - 0) / (height - 0)) * (1.0 - (-1.0));
 
-            points++;
-            addPoints(controlPoints, x, y, width, height);
-            controlPointsUpdated = true;
+                float rescaled_x2 = -1.0 + ((1.0*x - 0) / (width - 0)) * (1.0 - (-1.0));
+                float rescaled_y2 = -1.0 + ((1.0*(height - y) - 0) / (height - 0)) * (1.0 - (-1.0));
+                auto it = std::find (controlPoints.begin(), controlPoints.end(), rescaled_x1);
+                if (it != controlPoints.end())
+                {
 
-            if((points%3==1 && points>4) || points==4) {
-                endpoint = true;
-                int pS = pointsLoc.size();
-                float col_x = (2 * pointsLoc[pS - 2]) - pointsLoc[pS - 4];
-                float col_y = (2 * pointsLoc[pS - 1]) - pointsLoc[pS - 3];
-                points++;
-                addPoints(controlPoints, col_x, col_y, width, height);
-                controlPointsUpdated = true;
+                    controlPoints.at(it-controlPoints.begin()) = rescaled_x2;
+                    controlPoints.at(it-controlPoints.begin() + 1) = rescaled_y2;
+                    controlPointsUpdated = true;
+                    pointSelected = false;
+                    pointBuffer = false;
+                    placePoints.emplace_back(x,y);
+                    std::cout << "A point has been changed." << std::endl;
+                    endpoint = true;
+                    linearBezDraw = false;
+                }
+
+
             }
+
+            else{
+                for ( const auto& i : placePoints ) {
+                    float diff_x = abs(std::get<0>(i) - x);
+                    float diff_y = abs(std::get<1>(i) - y);
+                    float threshold = 30;
+//                    std::cout << diff_x << diff_y << std::endl;
+                    if(diff_x<=threshold && diff_y<=threshold)
+                    {
+                        std::cout << "Point exists." << std::endl;
+                        pointSelected = true;
+                        std::get<0>(selectedCoords) = std::get<0>(i);
+                        std::get<1>(selectedCoords) = std::get<1>(i);
+                    }
+                    else{
+//                        std::cout << "NOT Found" << std::endl;
+                        placePoints.emplace_back(x,y);
+                    }
+                }
+
+                if(pointSelected)
+                {
+                    float selected_x = std::get<0>(selectedCoords);
+                    float selected_y = std::get<1>(selectedCoords);
+
+                    float rescaled_x = -1.0 + ((1.0*selected_x - 0) / (width - 0)) * (1.0 - (-1.0));
+                    float rescaled_y = -1.0 + ((1.0*(height - selected_y) - 0) / (height - 0)) * (1.0 - (-1.0));
+                    pointBuffer = true;
+
+                }
+                else{
+                    if(placePoints.empty())
+                    {
+                        placePoints.emplace_back(x,y);
+                    }
+
+                    pointsLoc.push_back(x);
+                    pointsLoc.push_back(y);
+                    points++;
+                    addPoints(controlPoints, x, y, width, height);
+                    controlPointsUpdated = true;
+
+                    if((points%3==1 && points>4) || points==4) {
+                        endpoint = true;
+                        int pS = pointsLoc.size();
+                        float col_x = (2 * pointsLoc[pS - 2]) - pointsLoc[pS - 4];
+                        float col_y = (2 * pointsLoc[pS - 1]) - pointsLoc[pS - 3];
+                        points++;
+                        addPoints(controlPoints, col_x, col_y, width, height);
+                        controlPointsUpdated = true;
+                    }
+                }
+            }
+
          }
 
         if(controlPointsUpdated) {
@@ -183,7 +255,10 @@ int main(int, char* argv[])
             glEnableVertexAttribArray(0); //Enable first attribute buffer (vertices)
 
             // Update VAO/VBO for piecewise linear Bezier curve (since we added a new point)
-            calculatePiecewiseLinearBezier();
+            if(linearBezDraw){
+                calculatePiecewiseLinearBezier();
+            }
+
             glBindVertexArray(VAO_linearBezier);
             glBindBuffer(GL_ARRAY_BUFFER, VBO_linearBezier);
             glBufferData(GL_ARRAY_BUFFER, linearBezier.size()*sizeof(GLfloat), &linearBezier[0], GL_DYNAMIC_DRAW);
@@ -203,9 +278,10 @@ int main(int, char* argv[])
 
         glUseProgram(shaderProgram);
 
-        // Draw control points
+        // Draw control point\s
         glBindVertexArray(VAO_controlPoints);
 		glDrawArrays(GL_POINTS, 0, controlPoints.size()/3); // Draw points
+
 
 #if DRAW_CUBIC_BEZIER
         // TODO:
@@ -229,6 +305,7 @@ int main(int, char* argv[])
     // Delete VBO buffers
     glDeleteBuffers(1, &VBO_controlPoints);
     glDeleteBuffers(1, &VBO_linearBezier);
+    glDeleteBuffers(1, &VBO_cubicBezier);
     //TODO:
 
     // Cleanup
